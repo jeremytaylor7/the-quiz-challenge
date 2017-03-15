a46b152f09415879d848b214d3d0e95f2aa55629
Jeremy Taylor <goldplatedmarketer@gmail.com>
2017-03-14 22:22:35 -0700

    second commit more changes

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 
-var quizState = [
-    {questions:[]},
-    {answers:[]},
-    {results:},     
-]
+var quizState = {
+    questions:[],
+    answers:[],
+    results:[],
+    progress:[],
+    started: false
+}     
+
 
 
 
@@ -14,11 +17,30 @@ var quizState = [
 
 
 
+
+
 //render side
+function render() {
+	if(quizState.started) {
+		$('.question').show();
+		$('.js-start').hide();
+	} else {
+		$('.js-start').show();
+		$('.question').hide();
+	}
+}
 
 
 //event handlers
-
+function startHandler(e) {
+	e.preventDefault();
+	quizState.started = true;
+	render();
+}
+$(function() {
+	$('.js-start').click(startHandler);
+	render();
+})
 
 
 //callbacks
