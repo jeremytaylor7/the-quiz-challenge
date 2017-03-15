b3cd1f3ab110017ef8147dd4ccd78e1716e84927
Jeremy Taylor <goldplatedmarketer@gmail.com>
2017-03-14 20:34:43 -0700

    first commit skeleton

---
+++ app.js
@@ -0,0 +1,24 @@
+
+var quizState = [
+    {questions:[]},
+    {answers:[]},
+    {results:},     
+]
+
+
+
+
+//state management functions/basic functionality
+
+
+
+
+
+//render side
+
+
+//event handlers
+
+
+
+//callbacks
