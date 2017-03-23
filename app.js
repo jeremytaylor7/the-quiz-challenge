c0e90f26985cd35dae492d8049792a1bbc0be7bc
Jeremy Taylor <goldplatedmarketer@gmail.com>
2017-03-22 21:04:49 -0700

    removed red and green light functions, changed state properties by adding a question array and removing properties also added user story

--- app.js
+++ app.js
@@ -1,12 +1,40 @@
 
-var quizState = [{
-   	questions:[],
-   	answers: ['a', 'b', 'c', 'd'],
-    attempted: false,
+var quizState = {
+   	questions:['answer1',
+   	           'answer2',
+   	           'answer3',
+   	           'answer4',
+   	           'answer5'],
+   	correctAnswers: ['a', 'b', 'c', 'd'],
+   	started: false,
     right:0,
-    started: false
+    incorrect:0,
 
-}]     
+    
+   
+
+
+
+}
+
+/* As a user I would like to see be able to click a start
+   button to see a question displayed that I can answer 
+   by choosing the right answer out of 4 possible choices.
+
+   after choosing the right answer I want the program to
+   check if the answer was right or wrong and then display
+   on the screen if I was right or wrong and show what the
+   correct answer is if I was wrong. After I see the
+   results of my choice I want to be able to move on to
+   the next question and see another question displayed etc.
+
+   After I answer the last question I would like to be able
+   to see a screen with my score displayed on the screen and
+   then have the option to play again by clicking on the button
+   which would take me back to start screen.
+
+*/
+
 
 
 
@@ -14,20 +42,31 @@ var quizState = [{
 
 //state management functions/basic functionality
 
-function checkAnswer(q, i) {
 
-	var index = i;
+function startQuiz(){
 
-	if (q === quizState.answers[index].answer) {
+	quizState.started = true;
+}
+
+
+
+function checkAnswer(value) {
+
+
+	for (var i = 0; i <= 4; i++) {
+
+	if (value === quizState.correctAnswers[i]) {
+
 
 		quizState.right++;
-		turnGreen();
+		
 	}
 	else {
 
 		
-		turnRed();
+		
 	}
+}
 
 }
 
@@ -52,20 +91,6 @@ function renderAnswerResultss() {
 
 
 
-function turnGreen() {
-
-	$('.correct').addClass('turnGreen');
-
-
-};
-
-function turnRed() {
-
-	$('.incorrect').addClass('turnRed');
-
-};
-
-
 
 //event handlers
 function startHandler(e) {
@@ -79,7 +104,7 @@ $(function() {
 	render();
 	$('.choice > button').on('click', function(event) {
 		event.preventDefault();
-		value = parseInt($(this).attr('value'));
+		value = $(this).attr('value');
 		checkAnswer(value);
 
 	});
